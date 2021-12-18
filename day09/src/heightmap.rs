e04328ba623b923addc72861ce6ff770ccc926b6
faffeldt <felix.affeldt9698@gmail.com>
2021-12-18 21:28:48 +0100

    Adds solution for day09a

--- day09/src/heightmap.rs
+++ day09/src/heightmap.rs
@@ -33,6 +33,53 @@ impl HeightMap {
 
         HeightMap::new(heightmap, width, height)
     }
+
+    pub fn low_points(&self) -> Vec<usize> {
+        let mut low_points: Vec<usize> = Vec::new();
+        
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let current = self.heightmap[y * self.width + x];
+                let mut is_low_point = true;
+
+                if x > 0 {
+                    // Check left
+                    is_low_point &= current < self.heightmap[y * self.width + (x - 1)]
+                }
+                
+                if x < self.width - 1 {
+                    // Check right
+                    is_low_point &= current < self.heightmap[y * self.width + (x + 1)]
+                }
+
+                if y > 0 {
+                    // Check above
+                    is_low_point &= current < self.heightmap[(y - 1) * self.width + x]
+                }
+
+                if y < self.height - 1 {
+                    // Check below
+                    is_low_point &= current < self.heightmap[(y + 1) * self.width + x]
+                }
+                
+                if is_low_point {
+                    // println!{"DEBUG> Found low point at x:{} y:{} with value {}", x, y, current};
+                    low_points.push(current);
+                }
+            }
+        }
+        low_points
+    }
+
+    pub fn risk_of_height(height: usize) -> usize {
+        1 + height
+    }
+
+    pub fn total_risk(&self) -> usize {
+        self.low_points().iter()
+            .map(|&p| HeightMap::risk_of_height(p))
+            .fold(0, |acc, r| acc + r)
+    }
 }
 
 impl fmt::Display for HeightMap {
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -6,7 +6,7 @@ use crate::heightmap::HeightMap;
 pub fn main() {
     println!("CWD: {:?}", env::current_dir());
     local_test_a();
-    // solution_a();
+    solution_a();
     // local_test_b();
     // solution_b();
 }
@@ -17,46 +17,65 @@ fn local_test_a() {
     let heightmap = HeightMap::parse_input(input);
     println!{"TEST A> Input:\n{}", heightmap};
 
-    
-    
-    // println!{"Test A> Result {}: {:?}", result, result_numbers};
+    let expected_low_points = vec![1, 0, 5, 5];
+    let low_points = heightmap.low_points();
+    assert_eq!{expected_low_points, low_points, "TEST A> Low points mismatch: {:?} == {:?}", expected_low_points, low_points};
+
+    let expected_risks = vec![2, 1, 6, 6];
+    let risks: Vec<usize> = low_points.iter()
+        .map(|&p| HeightMap::risk_of_height(p))
+        .collect();
+    assert_eq!{expected_risks, risks, "TEST A> Risks mismatch: {:?} == {:?}", expected_risks, risks};
+
+    let expected_total_risk = 15;
+    // let total_risk = risks.iter().fold(0, |acc, r| acc + r);
+    let total_risk = heightmap.total_risk();
+    assert_eq!{expected_total_risk, total_risk, "TEST A> Total risk mismatches: {} == {}", expected_total_risk, total_risk};
+
+    println!{"Test A> Total risk: {}", total_risk};
 }
 
-// fn solution_a() {
-//     let input_file = ".\\solution_input.txt";
-//     let input = fs::read_to_string(&input_file).unwrap_or_else(|_| panic!("Error reading file {}", input_file));
-    
-//     println!{"Solution A> Result {}: {:?}", result, result_numbers};
-// }
+fn solution_a() {
+    let input_file = ".\\solution_input.txt";
+    let input = fs::read_to_string(&input_file).unwrap_or_else(|_| panic!("Error reading file {}", input_file));
+    let heightmap = HeightMap::parse_input(input);
+    println!{"SOLUTION A> Input:\n{}", heightmap};
+
+    let total_risk = heightmap.total_risk();
+    println!{"SOLUTION A> Total risk: {}", total_risk};
+}
 
 // fn local_test_b() {
-//     // Perform tests with simple input string
-//     let test_input = String::from("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf");
-//     let test_measurements = Measurement::parse_input(test_input);
-   
-//     println!{"DEBUG B> Test result {:?}", test_result_numbers};
-//     let expected_test_result = 5353;
-//     assert_eq!{test_result_numbers[0], expected_test_result, "DEBUG B> Mismatched test result: {} == {}", test_result_numbers[0], expected_test_result}
-    
-//     // Perform tests with test input file
 //     let input_file = ".\\test_input.txt";
 //     let input = fs::read_to_string(&input_file).unwrap_or_else(|_| panic!("Error reading file {}", input_file));
-//     let measurements = Measurement::parse_input(input);
-    
-//     println!{"TEST B> Test result {:?}", result_numbers};
-    
-    
-//     let result = result_numbers.iter().fold(0, |acc, n| acc + n);
-//     let expected_result = 61229;
-//     assert_eq!{result, expected_result, "TEST B> Mismatched result: {} == {}", result, expected_test_result}
-//     println!{"TEST B> Result: {}", result};
+//     let heightmap = HeightMap::parse_input(input);
+//     println!{"TEST B> Input:\n{}", heightmap};
+
+//     let expected_low_points = vec![1, 0, 5, 5];
+//     let low_points = heightmap.low_points();
+//     assert_eq!{expected_low_points, low_points, "TEST B> Low points mismatch: {:?} == {:?}", expected_low_points, low_points};
+
+//     let expected_risks = vec![2, 1, 6, 6];
+//     let risks: Vec<usize> = low_points.iter()
+//         .map(|&p| HeightMap::risk_of_height(p))
+//         .collect();
+//     assert_eq!{expected_risks, risks, "TEST B> Risks mismatch: {:?} == {:?}", expected_risks, risks};
+
+//     let expected_total_risk = 15;
+//     // let total_risk = risks.iter().fold(0, |acc, r| acc + r);
+//     let total_risk = heightmap.total_risk();
+//     assert_eq!{expected_total_risk, total_risk, "TEST B> Total risk mismatches: {} == {}", expected_total_risk, total_risk};
+
+//     println!{"Test B> Total risk: {}", total_risk};
 // }
 
 // fn solution_b() {
 //     let input_file = ".\\solution_input.txt";
 //     let input = fs::read_to_string(&input_file).unwrap_or_else(|_| panic!("Error reading file {}", input_file));
-    
-//     let result = result_numbers.iter().fold(0, |acc, n| acc + n);
-//     println!{"SOLUTION B> Result: {}", result};
+//     let heightmap = HeightMap::parse_input(input);
+//     println!{"SOLUTION B> Input:\n{}", heightmap};
+
+//     let total_risk = heightmap.total_risk();
+//     println!{"SOLUTION B> Total risk: {}", total_risk};
 // }
 
