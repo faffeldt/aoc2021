86dd1e24f72963e6f2ae3193a30e394262392108
faffeldt <felix.affeldt9698@gmail.com>
2021-12-28 12:00:57 +0100

    Refactors day14 for b part

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -2,13 +2,14 @@ use std::env;
 use std::fs;
 mod polymerization;
 use crate::polymerization::PolymerTemplate;
+use std::collections::BTreeMap;
 
 pub fn main() {
     println!("CWD: {:?}", env::current_dir());
-    // local_test_a();
-    // solution_a();
+    local_test_a();
+    solution_a();
     local_test_b();
-    // solution_b();
+    solution_b();
 }
 
 fn local_test_a() {
@@ -24,18 +25,59 @@ fn local_test_a() {
         "NNCB",
         "NCNBCHB",
         "NBCCNBBBCBHCB",
-        "NBBBCNCCNBBNBNBBCHBHHBCHB",
-        "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB",
+        // "NBBBCNCCNBBNBNBBCHBHHBCHB",
+        // "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB",
     ];
-    for i in 0..expected_step_states.len() {
-        assert_eq!{expected_step_states[i].chars().collect::<Vec<char>>(), polymer_template.states[0], "Polymer states in step {} mismatch", i};
+
+    let expected_step_states_2 = vec![
+        BTreeMap::from([(('N', 'N'), 1), (('N', 'C'), 1), (('C', 'B'), 1)]),
+        BTreeMap::from([
+            (('N', 'C'), 1),
+            (('C', 'N'), 1),
+            (('N', 'B'), 1),
+            (('B', 'C'), 1),
+            (('C', 'H'), 1),
+            (('H', 'B'), 1),
+        ]),
+        BTreeMap::from([
+            (('N', 'B'), 2),
+            (('B', 'C'), 2),
+            (('C', 'C'), 1),
+            (('C', 'N'), 1),
+            (('B', 'B'), 2),
+            (('C', 'B'), 2),
+            (('B', 'H'), 1),
+            (('H', 'C'), 1),
+        ]),
+    ];
+
+    for (i, step) in expected_step_states.iter().enumerate() {
+        // Prepare expected step data for comparison
+        let mut count_nums: BTreeMap<char, usize> = BTreeMap::new();
+        let mut pair_nums: BTreeMap<(char, char), usize> = BTreeMap::new();
+        let step_chars = step.chars().collect::<Vec<char>>();
+        step_chars
+            .iter()
+            .for_each(|c| *count_nums.entry(*c).or_insert(0) += 1);
+        step_chars
+            .windows(2)
+            .for_each(|window| *pair_nums.entry((window[0], window[1])).or_insert(0) += 1);
+
+        // Verify that the preparation worked
+        assert_eq! {pair_nums, expected_step_states_2[i], "Pair nums mismatch in step {}", i};
+
+        // Verify that the expected states match actual state
+        assert_eq! {count_nums, polymer_template.counts, "Polymer states in step {} mismatch", i};
+
+        // Evolute the polymer for next test
         polymer_template.build_polymer();
+        println! {"DEBUG> Polymer after step {}:\n  counts={:?}\n  pairs={:?}", i, polymer_template.counts, polymer_template.pairs};
     }
 
     let expected_result = 1588;
     polymer_template.build_polymer_n_times(10 - expected_step_states.len());
     let result = polymer_template.score_of_last();
-    assert_eq!{expected_result, result, "TEST A> Result of last polymer score mismatches"};
+    assert_eq! {expected_result, result, "TEST A> Result of last polymer score mismatches"};
 }
 
 fn solution_a() {
@@ -47,7 +89,7 @@ fn solution_a() {
     println! {"SOLUTION A> Input:\n{}", polymer_template};
     polymer_template.build_polymer_n_times(10);
     println! {"SOLUTION A> After 10 steps:\n{}", polymer_template};
-    
+
     let result = polymer_template.score_of_last();
     println! {"Solution A> Score of last polymer {}", result};
 }
@@ -63,7 +105,7 @@ fn local_test_b() {
 
     let expected_result = 2188189693529;
     let result = polymer_template.score_of_last();
-    assert_eq!{expected_result, result, "TEST B> Result of last polymer score mismatches"};
+    assert_eq! {expected_result, result, "TEST B> Result of last polymer score mismatches"};
 }
 
 fn solution_b() {
@@ -72,10 +114,10 @@ fn solution_b() {
         .unwrap_or_else(|_| panic!("Error reading file {}", input_file));
 
     let mut polymer_template = PolymerTemplate::parse_input(&input);
-    println! {"SOLUTION A> Input:\n{}", polymer_template};
+    println! {"SOLUTION B> Input:\n{}", polymer_template};
     polymer_template.build_polymer_n_times(40);
-    // println! {"SOLUTION A> After 40 steps:\n{}", polymer_template};
-    
+    println! {"SOLUTION B> After 40 steps:\n{}", polymer_template};
+
     let result = polymer_template.score_of_last();
     println! {"Solution A> Score of last polymer {}", result};
 }
--- day14/src/polymerization.rs
+++ day14/src/polymerization.rs
@@ -1,19 +1,23 @@
+use std::collections::BTreeMap;
 use std::fmt;
-use std::collections::HashMap;
+use std::mem;
 use std::time::{Duration, Instant};
 
 pub struct PolymerTemplate {
-    pub states: Vec<Vec<char>>,
-    pub insertion_rules: HashMap<(char, char), char>,
+    pub counts: BTreeMap<char, usize>,
+    pub pairs: BTreeMap<(char, char), usize>,
+    pub insertion_rules: BTreeMap<(char, char), char>,
 }
 
 impl PolymerTemplate {
     pub fn new(
-        initial_state: Vec<char>,
-        insertion_rules: HashMap<(char, char), char>
+        counts: BTreeMap<char, usize>,
+        pairs: BTreeMap<(char, char), usize>,
+        insertion_rules: BTreeMap<(char, char), char>,
     ) -> PolymerTemplate {
         PolymerTemplate {
-            states: vec![initial_state],
+            counts: counts,
+            pairs: pairs,
             insertion_rules: insertion_rules,
         }
     }
@@ -21,17 +25,17 @@ impl PolymerTemplate {
     pub fn parse_input(input: &str) -> PolymerTemplate {
         println! {">>> Starting to parse input <<<"};
 
-        let mut insertion_rules: HashMap<(char, char), char> = HashMap::new();
-        let mut initial_state: Vec<char> = Vec::new();
+        let mut insertion_rules: BTreeMap<(char, char), char> = BTreeMap::new();
+        let mut counts: BTreeMap<char, usize> = BTreeMap::new();
+        let mut pairs: BTreeMap<(char, char), usize> = BTreeMap::new();
+
         input.lines().for_each(|l| {
             if l.len() < 4 {
                 // Ignore split line
             } else if l.contains("->") {
                 // lines containing insertion rules like:
                 // CH -> B
-                let raw_instr: Vec<&str> = l
-                    .split(" -> ")
-                    .collect::<Vec<&str>>();
+                let raw_instr: Vec<&str> = l.split(" -> ").collect::<Vec<&str>>();
                 let mut pattern = raw_instr[0].chars();
                 let insertion_character = raw_instr[1].chars().next().unwrap();
                 insertion_rules
@@ -39,63 +43,61 @@ impl PolymerTemplate {
                     .or_insert(insertion_character);
             } else {
                 // Base template
-                initial_state = l.to_string().chars().collect();
+                let chars = l.to_string().chars().collect::<Vec<char>>();
+                chars
+                    .iter()
+                    .for_each(|c| *counts.entry(*c).or_insert(0) += 1);
+                chars
+                    .windows(2)
+                    .for_each(|w| *pairs.entry((w[0], w[1])).or_insert(0) += 1);
             }
         });
-        
-        PolymerTemplate::new(initial_state, insertion_rules)
+
+        PolymerTemplate::new(counts, pairs, insertion_rules)
     }
 
     pub fn build_polymer(&mut self) {
-        // println! {"DEBUG> Start building polymer"};
-        let last_state = self.states.last().unwrap();
-        let mut new_state: Vec<char> = Vec::new();
+        println! {"DEBUG> Start building polymer"};
+        let mut new_state: BTreeMap<(char, char), usize> = BTreeMap::new();
+        for ((c1, c2), num) in &self.pairs {
+            let ic = self.insertion_rules.get(&(*c1, *c2)).unwrap();
+            // Update the counts of the cells in polymer with inserted char
+            *self.counts.entry(*ic).or_insert(0) += num;
 
-        last_state.windows(2).for_each(
-            |window| {
-                let first = window[0];
-                let second = window[1];
-                // new_state.push(first);
+            // Update the number of pairs in new state
+            *new_state.entry((*c1, *ic)).or_insert(0) += num;
+            *new_state.entry((*ic, *c2)).or_insert(0) += num;
 
-                match self.insertion_rules.get(&(first, second)) {
-                    Some(i) => {
-                        new_state.push(first);
-                        new_state.push(*i);
-                    },
-                    None    => new_state.push(first),
-                }
-            }
-        );
-        new_state.push(*last_state.last().unwrap());
-        // self.states.push();
-        self.states = vec![new_state];
-        // println! {"DEBUG> Finished building polymer\n\t{}", self.states[0].iter().collect::<String>()};
+            // println!{
+            //     "DEBUG>\tPair={:?} -> ({}, {}) and ({}, {})\n\tCounter: {:?}",
+            //     ((c1, c2), num), c1, ic, ic, c2,
+            //     new_state,
+            // };
+        }
+        self.pairs = new_state;
     }
 
     pub fn build_polymer_n_times(&mut self, n: usize) {
-        println!{"DEBUG> Starting to perform {} steps", n};
+        println! {"DEBUG> Starting to perform {} steps", n};
         let now = Instant::now();
         for i in 0..n {
-            let now2 = Instant::now();
             self.build_polymer();
-            println!{"DEBUG> Finished step {} with {} chars in polymer in {} secs (Total: {} secs)",
-                      i, self.states.last().unwrap().len(), now2.elapsed().as_secs(), now.elapsed().as_secs()};
+            println! {"DEBUG> Polymer after step {}: counts={:?} pairs={:?}", i, self.counts, self.pairs};
         }
-        println!{"DEBUG> Finished performing {} steps in {} sec", n, now.elapsed().as_secs()};
+        println! {"DEBUG> Finished performing {} steps in {} sec", n, now.elapsed().as_secs()};
     }
 
     pub fn score_of_last(&self) -> usize {
-        let last_polymer = self.states.last().unwrap();
-        let mut counts: HashMap<char, usize> = HashMap::new();
-        last_polymer.iter()
-            .for_each(|c| *counts.entry(*c).or_insert(1) += 1);
-        
-        println!{"DEBUG> Counts of last: {:?}", counts};
-        let mut min_count = 999999;
+        let mut min_count = usize::MAX;
         let mut max_count = 0;
-        for (_, count) in counts {
-            if count < min_count {min_count = count};
-            if count > max_count {max_count = count};
+
+        for (_, count) in &self.counts {
+            if *count < min_count {
+                min_count = *count
+            };
+            if *count > max_count {
+                max_count = *count
+            };
         }
         max_count - min_count
     }
@@ -103,10 +105,12 @@ impl PolymerTemplate {
 
 impl fmt::Display for PolymerTemplate {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln! {f, "PolymerTemplate insertion rules: {:?} and states:", self.insertion_rules};
-        for (i, state) in self.states.iter().enumerate() {
-            writeln!{f, "After step {}: {}", i, state.into_iter().collect::<String>()};
-        }
-        write!{f, ""}
+        writeln! {f,
+        "PolymerTemplate counts: {:?}\n\tstates: {:?}\n\tinsertion rules: {:?}",
+        self.counts, self.pairs, self.insertion_rules};
+        // for (i, state) in self.states.iter().enumerate() {
+        //     writeln!{f, "After step {}: {}", i, state.into_iter().collect::<String>()};
+        // }
+        write! {f, ""}
     }
 }
